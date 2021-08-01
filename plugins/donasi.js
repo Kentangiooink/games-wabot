b6bf5323ef1c935c38ef6c78dd5fbb62d7160dd0
Kentangiooink <88274542+Kentangiooink@users.noreply.github.com>
2021-08-01 20:12:42 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,10 +1,32 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ • Indosat [085713964963]
-│ • Gopay [085713964963]
+
+│ • Telkomsel [082217590187]
+
+│ • Dana [082217590187]
+
 ╰────
-╭─「 Hubungi 」
-│ > Ingin donasi? Wa.me/6281390658325
+
+╭─「 INGFO OWNER 」
+
+│ • NAME : MICHAEL AGAM
+
+│ • CONTACT : 082217590187
+
+│                        081283815593
+
+│ • WHATSAPP : wa.me/6282217590187
+
+│ • YOUTUBE : MICHAEL OFC
+
+│ *PLEASE SUBSCRIBE MY CHANNEL
+
+│   MICHAEL OFC*
+
+│ 
+
+│ _THANKS_
+
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']

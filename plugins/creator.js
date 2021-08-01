34d7e7260120d4bd84d88874238ce94cd64211bc
Kentang0323 <88254438+Kentang0323@users.noreply.github.com>
2021-08-01 15:31:31 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 let handler = function (m) {
-  // this.fakeReply(m.chat, 'This is my owner', '6281390658325@s.whatsapp.net', 'Owner')
+  // this.fakeReply(m.chat, 'This is my owner', '6282217590187@s.whatsapp.net', 'Owner')
   this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
-  this.sendContact(m.chat, '6281390658325', 'Owner Metro Bot :)', m)
+  this.sendContact(m.chat, '6282217590187', 'Lord Michael', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
